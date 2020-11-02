9fcbe3dac0fd658e3f322836daacbe96be9aa55f
Lia Coleman <liailiad@gmail.com>
2020-11-02 01:24:30 -0600

    play button

--- assets/js/script.js
+++
@@ -1,9 +0,0 @@
-// const frameKeyword = 'frame';
-
-const urlParams = new URLSearchParams(window.location.search);
-const myParam = urlParams.get('frame');
-
-
-staticFrame = (myParam === "")
-console.log(myParam);
-console.log(staticFrame);
--- assets/js/sketch.js
+++ assets/js/sketch.js
@@ -1,30 +1,52 @@
 let playing = false;
 let movie;
+let button;
 
 // CONSTS
 const videoFile = "assets/img/4x-resolve-music-sq.mp4";
 const frameKeyword = "frame";
 
 function setup() {
-  movie = createVideo([videoFile]);
+  movie = createVideo([videoFile], vidLoad);
+  movie.size(AUTO, windowHeight).center("horizontal");
+}
 
-  if (isStaticFrame()){
+function vidLoad() {
+  if (!isStaticFrame()){
+    // movie.play();
+    button = createButton('play');
+    button.mousePressed(toggleVid); // attach button listener
 
-  	secondsF = movie.duration();
-  	console.log(secondsF);
-  	if (secondsF > 0) {
+  } else {
+
+    // STATIC FRAME
+    secondsF = movie.duration();
+    console.log(secondsF);
+    if (secondsF > 0) {
       let randomTime = random(floor(secondsF));
       movie.time(randomTime);
     }
-
-  } else {
-  	console.log("VIDEO");
-  	movie.play().loop().time(0);
   }
-
 }
+
+// plays or pauses the video depending on current state
+function toggleVid() {
+  if (playing) {
+    movie.pause();
+    button.html('play');
+  } else {
+    movie.loop();
+    button.html('pause');
+  }
+  playing = !playing;
+}
+
 
 function isStaticFrame(){
   const urlParams = new URLSearchParams(window.location.search);
   return urlParams.get(frameKeyword) === "";
+}
+
+function windowResized() {
+  movie.size(AUTO, windowHeight).center("horizontal");
 }
